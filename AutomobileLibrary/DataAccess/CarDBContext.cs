099872bfd1cdbed94c024df493927d1bde85f561
Hiu2411 <97373938+Hiu2411@users.noreply.github.com>
2022-06-14 16:35:26 +0700

    Add files via upload

---
+++ AutomobileLibrary/DataAccess/CarDBContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomobileLibrary.BussinessObject;
+namespace AutomobileLibrary.DataAccess
+{
+    public class CarDBContext
+    {
+        //Initialize car list 
+        private static List<Car> Carlist = new List<Car>(){
+            new Car{ CarID=1, CarName="CRV", Manufacturer="Honda",
+                Price=30000, ReleaseYear=2021 },
+            new Car{ CarID=2, CarName="Ford Focus", Manufacturer="Ford",
+                Price=15000, ReleaseYear=2020 }
+        };
+        //------- ------------------------------------ 
+        //Using Singleton Pattern 
+        private static CarDBContext instance = null;
+        private static readonly object instanceLock = new object();
+        private CarDBContext() { }
+        public static CarDBContext Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new CarDBContext();
+                    }
+                    return instance;
+                }
+            }
+        }
+        //--------------------------------------
+        public List<Car> GetCarlist => Carlist;
+        //----- 
+        public Car GetCarByID(int carID)
+        {
+            //using LINQ to Object 
+            Car car = Carlist.SingleOrDefault(pro => pro.CarID == carID);
+            return car;
+        }
+        //----------------------- 
+        //Add new a car 
+        public void AddNew(Car car)
+        {
+            Car pro = GetCarByID(car.CarID);
+            if (pro == null)
+            {
+                Carlist.Add(car);
+            }
+            else
+            {
+                throw new Exception("Car is already exists.");
+            }
+        }
+        //Update a car 
+        public void Update(Car car)
+        {
+            Car c = GetCarByID(car.CarID);
+            if (c != null)
+            {
+                var index = Carlist.IndexOf(c);
+                Carlist[index] = car;
+            }
+            else
+            {
+                throw new Exception("Car does not already exists.");
+            }
+        }
+        //-------
+        //Remove a car 
+        public void Remove(int CarID)
+        {
+            Car p = GetCarByID(CarID);
+            if (p != null)
+            {
+                Carlist.Remove(p);
+            }
+            else
+            {
+                throw new Exception("Car does not already exists.");
+            }
+        }//end Remove
+    }
+}
---
+++ AutomobileWinApp/frmCarDetails.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AutomobileWinApp
+{
+    partial class frmCarDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCarID = new System.Windows.Forms.Label();
+            this.lbCarName = new System.Windows.Forms.Label();
+            this.lbManufacturer = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.lbReleaseYear = new System.Windows.Forms.Label();
+            this.txtCarID = new System.Windows.Forms.TextBox();
+            this.txtCarName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.MaskedTextBox();
+            this.txtReleaseYear = new System.Windows.Forms.MaskedTextBox();
+            this.cboManufacturer = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbCarID
+            // 
+            this.lbCarID.AutoSize = true;
+            this.lbCarID.Location = new System.Drawing.Point(34, 34);
+            this.lbCarID.Name = "lbCarID";
+            this.lbCarID.Size = new System.Drawing.Size(39, 15);
+            this.lbCarID.TabIndex = 0;
+            this.lbCarID.Text = "Car ID";
+            // 
+            // lbCarName
+            // 
+            this.lbCarName.AutoSize = true;
+            this.lbCarName.Location = new System.Drawing.Point(34, 65);
+            this.lbCarName.Name = "lbCarName";
+            this.lbCarName.Size = new System.Drawing.Size(60, 15);
+            this.lbCarName.TabIndex = 1;
+            this.lbCarName.Text = "Car Name";
+            // 
+            // lbManufacturer
+            // 
+            this.lbManufacturer.AutoSize = true;
+            this.lbManufacturer.Location = new System.Drawing.Point(34, 95);
+            this.lbManufacturer.Name = "lbManufacturer";
+            this.lbManufacturer.Size = new System.Drawing.Size(79, 15);
+            this.lbManufacturer.TabIndex = 2;
+            this.lbManufacturer.Text = "Manufacturer";
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Location = new System.Drawing.Point(34, 129);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(33, 15);
+            this.lbPrice.TabIndex = 3;
+            this.lbPrice.Text = "Price";
+            // 
+            // lbReleaseYear
+            // 
+            this.lbReleaseYear.AutoSize = true;
+            this.lbReleaseYear.Location = new System.Drawing.Point(34, 162);
+            this.lbReleaseYear.Name = "lbReleaseYear";
+            this.lbReleaseYear.Size = new System.Drawing.Size(68, 15);
+            this.lbReleaseYear.TabIndex = 4;
+            this.lbReleaseYear.Text = "ReleaseYear";
+            // 
+            // txtCarID
+            // 
+            this.txtCarID.Location = new System.Drawing.Point(139, 26);
+            this.txtCarID.Name = "txtCarID";
+            this.txtCarID.Size = new System.Drawing.Size(216, 23);
+            this.txtCarID.TabIndex = 5;
+            // 
+            // txtCarName
+            // 
+            this.txtCarName.Location = new System.Drawing.Point(139, 57);
+            this.txtCarName.Name = "txtCarName";
+            this.txtCarName.Size = new System.Drawing.Size(216, 23);
+            this.txtCarName.TabIndex = 6;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(139, 121);
+            this.txtPrice.Mask = "000000000";
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(216, 23);
+            this.txtPrice.TabIndex = 7;
+            this.txtPrice.Text = "0";
+            // 
+            // txtReleaseYear
+            // 
+            this.txtReleaseYear.Location = new System.Drawing.Point(139, 154);
+            this.txtReleaseYear.Mask = "0000";
+            this.txtReleaseYear.Name = "txtReleaseYear";
+            this.txtReleaseYear.Size = new System.Drawing.Size(216, 23);
+            this.txtReleaseYear.TabIndex = 8;
+            this.txtReleaseYear.Text = "0";
+            // 
+            // cboManufacturer
+            // 
+            this.cboManufacturer.FormattingEnabled = true;
+            this.cboManufacturer.Items.AddRange(new object[] {
+            "Audi",
+            "BMW",
+            "Ford",
+            "Honda",
+            "Hyundai",
+            "Kia",
+            "Suzuki",
+            "Toyota"});
+            this.cboManufacturer.Location = new System.Drawing.Point(139, 87);
+            this.cboManufacturer.Name = "cboManufacturer";
+            this.cboManufacturer.Size = new System.Drawing.Size(216, 23);
+            this.cboManufacturer.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Location = new System.Drawing.Point(139, 197);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(280, 197);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 11;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmCarDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 276);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cboManufacturer);
+            this.Controls.Add(this.txtReleaseYear);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtCarName);
+            this.Controls.Add(this.txtCarID);
+            this.Controls.Add(this.lbReleaseYear);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.lbManufacturer);
+            this.Controls.Add(this.lbCarName);
+            this.Controls.Add(this.lbCarID);
+            this.Name = "frmCarDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCarDetails";
+            this.Load += new System.EventHandler(this.frmCarDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCarID;
+        private System.Windows.Forms.Label lbCarName;
+        private System.Windows.Forms.Label lbManufacturer;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label lbReleaseYear;
+        private System.Windows.Forms.TextBox txtCarID;
+        private System.Windows.Forms.TextBox txtCarName;
+        private System.Windows.Forms.MaskedTextBox txtPrice;
+        private System.Windows.Forms.MaskedTextBox txtReleaseYear;
+        private System.Windows.Forms.ComboBox cboManufacturer;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
